a2656345bbf51c15ceb07f5d6fcbcb47e247a60c
thenewhires <the.new.hires.public@gmail.com>
2026-01-31 22:04:34 +0100

    Add src/main/java/com/example/financetrackerbackend/FinanceTrackerBackendApplication.java

---
+++ src/main/java/com/example/financetrackerbackend/FinanceTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.financetrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceTrackerBackendApplication.class, args);
+	}
+
+}
